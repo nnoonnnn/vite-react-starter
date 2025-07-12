c1fdb1977913f076f386e086c20236502b4b563f
Nouar Chami <chaminouar37@gmail.com>
2025-07-12 06:12:08 +0200

    Initial Vite React project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="card">
+      <h1>Hello React</h1>
+      <p>This is a clean React + Vite project.</p>
+      <button onClick={() => alert('Clicked!')}>Click Me</button>
+    </div>
+  )
+}
+
+export default App
