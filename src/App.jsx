e3f39097606ffa20949ee93954db50fbac0a2cc4
binduthotal <bindus170@gmail.com>
2025-07-15 11:35:38 +0530

    Created Header and Footer Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import './App.css'
+import Header from './Header'
+import Footer from './Footer';
 
 function App() {
   return (
-    <div className="card">
-      <h1>Hello React</h1>
-      <p>This is a clean React + Vite project.</p>
-      <button onClick={() => alert('Clicked!')}>Click Me</button>
-      <footer style={{ textAlign: 'center', marginTop: '2rem' }}>
-  © 2025 vite-react-starter
-</footer>
-
+    <div className="app">
+      <Header />
+      <div className="card">
+        <h1>Hello React</h1>
+        <p>This is a clean React + Vite project.</p>
+        <button onClick={() => alert("Clicked!")}>Click Me</button>
+        {/* <footer style={{ textAlign: "center", marginTop: "2rem" }}>
+          © 2025 vite-react-starter
+        </footer> */}
+      </div>
+      <Footer />
     </div>
-  )
+  );
 }
 
 export default App
